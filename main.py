d6646a94daaf6c6717262d28e225bd718bbefed1
Farkhan Maulana <muhammad2000018280@webmail.uad.ac.id>
2023-06-13 23:02:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -84,4 +84,4 @@ async def predict_api(file: UploadFile = File(...)):
 
 # Test in local
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0',port=8001)
+    uvicorn.run(app, host='0.0.0.0',port=8080)
